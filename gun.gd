01ba8881ca9df83839f20408a30c667db3238886
cyan-vm <cyan.mv@gmail.com>
2024-01-21 14:38:45 -0600

    Make the gun detect aim at enemies

--- gun.gd
+++ gun.gd
@@ -2,5 +2,11 @@ extends Area2D
 
 func _physics_process(delta):
 	var enemies_in_range = get_overlapping_bodies()
-	if enemies_in_range.size() > 0: 
+	# print(enemies_in_range)
+	# print(enemies_in_range.size())
+	if enemies_in_range.size() > 0:
+		# print(enemies_in_range)
 		var target_enemy = enemies_in_range[0]
+		look_at(target_enemy.global_position)
+
+
